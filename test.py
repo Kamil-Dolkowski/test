fcc2cbabb826555d0acbc64c92d7628de7a1f90f
Kamil Dolkowski <kamil.dolkowski@op.pl>
2023-11-01 22:40:15 +0100

    k

--- test.py
+++ test.py
@@ -1,1 +1,60 @@
-print("Hello, world")
+# text=input("Podaj tekst: ")
+# text1=text.replace("a","x")
+# print(text1)
+
+# zdanie=input("Podaj zdanie: ")
+# print(zdanie.strip())
+
+# text=input("Podaj tekst: ")
+# count=text.count("a")
+# print(count)
+
+# text=input("Podaj tekst: ")
+# text1=text.capitalize()
+# print(text1)
+
+# text=input("Podaj tekst: ")
+# text1=text[::-1]
+# print(text1)
+
+# import random
+# lista=[random.randint(0,100) for _ in range(10)]
+# print(lista)
+
+# owoce={
+#     "jabłko":"czerwony",
+#     "banan":"żółty",
+#     "kiwi":"zielony"
+# }
+
+# for owoc in owoce.values():
+#     print(owoc)
+
+# imiona=["Anna","Kamil","Basia","Weronika"]
+# imiona.sort()
+# print(imiona)
+
+# osoby={
+#     "Anna":12,
+#     "Kamil":19
+# }
+
+# for osoba,wiek in osoby.items():
+#     if wiek>=18:
+#         print(osoba)
+
+# for wiek in osoby.items():
+#     print(wiek)
+
+# import random
+# import statistics
+# liczby = [random.randint(1, 50) for k in range(10)]
+# print(liczby)
+
+ceny={
+    "banan":1.99,
+    "jabłko":1.50,
+    "kiwi":2.50
+    }
+
+ceny["kiwi"]
